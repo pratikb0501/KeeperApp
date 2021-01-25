da14f35e0328e88b55173f430d6809d2b5040361
pratikb0501 <pratikbagmare@gmail.com>
2021-01-25 20:49:42 +0530

    added notes using map function

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,21 @@ import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import Note from "./Note";
+import notes from "./notes";
+
 
 function App() {
-    return <div><Header /><Note /><Footer /></div>
+    return <div>
+        <Header />
+        {notes.map(noteitem => <Note
+            key={noteitem.key}
+            title={noteitem.title}
+            content={noteitem.content}
+        />
+        )
+        }
+
+        <Footer /></div>
 }
 
 export default App;
